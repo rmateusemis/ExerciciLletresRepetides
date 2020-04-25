7b2ef95763e85ecfe7bee22126b5c97173c6b721
Roger Mateu <59694834+rmateusemis@users.noreply.github.com>
2020-04-25 12:32:32 +0200

    Update fase03.js

--- fase03.js
+++ fase03.js
@@ -1,7 +1,7 @@
 /*
 Emmagatzemar en un Map tant les lletres de la llista (o array) com el nombre de vegades que apareixen
 */
-/*
+
 function enviar(){
 
 var letters = ["R", "0", "G", "E", "R"];
@@ -9,23 +9,11 @@ var letters = ["R", "0", "G", "E", "R"];
 // Aplicamos reduce() al array letters
 // Param1: countLetter es el objeto donde guardo la cantidad de veces que se repite una letra
 // Param2: letter representa cada elemento del array.
-var numLetters = letters.reduce(function(countLetter, letter){
-  countLetter[letter] = (countLetter[letter] || 0) + 1; // Inicia en 0 y suma 1 por iteracion
-  return countLetter; // Retornamos el valor de countLetter
-}, {}); // countLetter es un array vacío
-
-console.log(numLetters);
-}
-*/
-
-function enviar(){
-
-var letters = ["R", "0", "G", "E", "R"];
 
 var numLetters = letters.reduce(function(countLetter, letter){
-  countLetter[letter] = (countLetter[letter] || 0) + 1;
-  return countLetter;
-}, {})
+  countLetter[letter] = (countLetter[letter] || 0) + 1; // Crea el valor en 0 y suma 1 por iteracion repetida (acumula)
+  return countLetter; // Retornamos el valor de countLetter (acumulador)
+}, {}); // countLetter es un objeto vacío
 
 console.log(numLetters);
 }
