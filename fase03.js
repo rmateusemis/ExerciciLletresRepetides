c8511db7ff8e8294ce6c18a783709f6d915e5fb1
Roger Mateu <59694834+rmateusemis@users.noreply.github.com>
2020-04-28 17:32:00 +0200

    Update fase03.js

--- fase03.js
+++ fase03.js
@@ -2,6 +2,25 @@
 Emmagatzemar en un Map tant les lletres de la llista (o array) com el nombre de vegades que apareixen
 */
 
+function enviar() {
+
+  var letters = ["R", "0", "G", "E", "R"];
+  var countLetter = {};
+
+  letters.map(function (letter) {
+    if (letter in countLetter) {
+      countLetter[letter]++
+    } else {
+      countLetter[letter] = 1
+    }
+    return countLetter;
+  },);
+
+  console.log(countLetter);
+}
+
+
+/*
 function enviar(){
 
 var letters = ["R", "0", "G", "E", "R"];
@@ -11,9 +30,10 @@ var letters = ["R", "0", "G", "E", "R"];
 // Param2: letter representa cada elemento del array.
 
 var numLetters = letters.reduce(function(countLetter, letter){
-  countLetter[letter] = (countLetter[letter] || 0) + 1; // Crea el valor en 0 y suma 1 por iteracion repetida (acumula)
-  return countLetter; // Retornamos el valor de countLetter (acumulador)
+countLetter[letter] = (countLetter[letter] || 0) + 1; // Crea el valor en 0 y suma 1 por iteracion repetida (acumula)
+return countLetter; // Retornamos el valor de countLetter (acumulador)
 }, {}); // countLetter es un objeto vacío
 
 console.log(numLetters);
 }
+*/
